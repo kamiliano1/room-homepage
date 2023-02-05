78c3f54d1696ac9d25ce9288494fcd6b13331eeb
kamiliano1 <kamiliano1991@gmail.com>
2023-02-05 18:31:40 +0100

    File preparations
    Prepared files for the new project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="text-red-300">
+      Test
+    </div>
+  )
+}
+
+export default App
